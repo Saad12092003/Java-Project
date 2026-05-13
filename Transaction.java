60904ff98dfe12899de3058a6a057bd87761389c
Sayma Siddiqa <am.siddiqa424@gmail.com>
2026-05-13 22:32:12 +0600

    Final fix for Transaction class

--- Transaction.java
+++ Transaction.java
@@ -2,7 +2,7 @@ class Transaction {
     public void transfer(Customer sender, Customer receiver, double amount) {
         if (sender.getBalance() >= amount) {
             sender.withdraw(amount);
-            receiver.Deposite(amount); // Ekhane 'e' add koro
+            receiver.Deposite(amount);
             System.out.println("Transfer Success!");
         } else {
             System.out.println("Insufficient Balance!");
