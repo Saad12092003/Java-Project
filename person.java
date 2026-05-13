a614f7f9fb7292e495777bd879754fa8774f71d2
saad12092003 <shaadbhuiyan2003@gmail.com>
2026-05-13 09:37:51 +0600

    modify the proteced variable of person class

--- person.java
+++ person.java
@@ -1,35 +1,28 @@
  abstract class person
 {
-    protected int AcountNumber;
-   protected String AcountHolder;
-   protected double balance;
-   public String employeName;
-   public String employeRole;
+    protected  int AcountNumber;
+   protected  String AcountHolder;
+   protected  double balance;
+    String employeName;
+    String employeRole;
+   
+   person(int AcountNumber,String AccountHolder,double balance)
+   {
+    this.AcountNumber=AcountNumber;
+    this.AcountHolder=AccountHolder;
+    this.balance=balance;
+   }
+   
    person(String employeName,String employeRole)
    {
      this.employeName=employeName;
      this.employeRole=employeRole;
    }
-   person(int AcountNumber,String AccountHolder,int balance)
-   {
-
-    this.AcountHolder=AccountHolder;
-    this.balance=balance;
-   }
    
-   public String getname()
-   {
-     return this.AcountHolder;
-   }
-   public int getnumber()
-   {
-    return this.AcountNumber;
-   }
-   public double getbalance()
-   {
-    return this.balance;
-   }
+   
 
    abstract void display(); 
+   
+   
 
 }
