13bb1f97b75e1f053941fb74ffb4298d60bf0332
saad12092003 <shaadbhuiyan2003@gmail.com>
2026-05-13 15:46:45 +0600

    Modify to Customer class and person class

--- Customer.java
+++ Customer.java
@@ -1,45 +1,21 @@
 
  class Customer extends person
 {
-     void setCustomerNumber(int AccountNumber)
-    {
-        this.AccountNumber=AccountNumber;
-    }
-    void setCustomerName(String AccountHolder)
-    {
-        this.AccountHolder=AccountHolder;
-    }
-    void setCustomerBalance(double balance)
-    {
-        this.balance=balance;
-    }
-
-    public int getNumber()
-    {
-        return this.AccountNumber;
-    }
-    public String getCustomerName()
-    {
-        return this.AccountHolder;
-    }
-    public double getBalance()
-    {
-        return this.balance;
-    }
+    
 
     void Deposite(double amount)
     {
         if(amount>=0)
         {
-            balance+=amount;
+           setCustomerBalance(getBalance() + amount);
         }
     }
 
 
     void withdraw(double amount){
-        if(amount >0 && amount <=balance)
+        if(amount >0 && amount <=getBalance())
         {
-            balance-=amount;
+            setCustomerBalance(getBalance() - amount);
         }
         else
         {
--- person.java
+++ person.java
@@ -1,10 +1,36 @@
 
  abstract class person {
-     protected int AccountNumber;
-     protected String AccountHolder;
-     protected double balance;
+     private int AccountNumber;
+     private String AccountHolder;
+     private double balance;
       String employeeName;
       String employeeRole;
+
+       void setCustomerNumber(int AccountNumber)
+    {
+        this.AccountNumber=AccountNumber;
+    }
+    void setCustomerName(String AccountHolder)
+    {
+        this.AccountHolder=AccountHolder;
+    }
+    void setCustomerBalance(double balance)
+    {
+        this.balance=balance;
+    }
+
+    public int getNumber()
+    {
+        return this.AccountNumber;
+    }
+    public String getCustomerName()
+    {
+        return this.AccountHolder;
+    }
+    public double getBalance()
+    {
+        return this.balance;
+    }
 
      abstract void display();
 
