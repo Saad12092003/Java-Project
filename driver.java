bc0b46f5a948131e7e9f98f777d1ee23fa327097
saad12092003 <shaadbhuiyan2003@gmail.com>
2026-05-13 09:48:13 +0600

    Draft Driver class upload into git hub

---
+++ driver.java
@@ -0,0 +1,14 @@
+public class driver
+{
+    public static void main(String[] args) {
+        person c =new Customer(101,"saad",5000);
+
+        person e =new employee("Forid","Acountent");
+        
+        c.display();
+        e.display();
+       
+       
+        
+    }
+}
