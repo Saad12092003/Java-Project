20f3fc9729ff7026ae172ea5ab622c7ec9014f33
saad12092003 <shaadbhuiyan2003@gmail.com>
2026-05-25 17:23:10 +0600

    Upate Customer class

--- Customer.java
+++ Customer.java
@@ -1,46 +1,51 @@
+import java.util.ArrayList;
 
- class Customer extends person
-{
+class Customer extends person {
+    private ArrayList<String> history = new ArrayList<>();
 
-    public Customer(int AccountNumber,String AccountHolder,double balance) {
+    public Customer(int AccountNumber, String AccountHolder, double balance) {
+
         super(AccountNumber, AccountHolder, balance);
-    }
-    
-    
 
-    void Deposite(double amount) throws InsufficientBalanceException
-    {
-        if(amount>=0)
-        {
+        history.add("Account Opened with: " + balance);
+    }
+
+    void Deposite(double amount) throws InsufficientBalanceException {
+
+        if (amount > 0) {
             updateBalance(amount);
-          
+
+         history.add("Deposited: " + amount + " | Current Balance: " + getBalance());
+        } else {
+
+            throw new InsufficientBalanceException("Invalid Deposit Amount");
         }
-        else
-        {
-            throw new InsufficientBalanceException("Isufficient Balance");
-        }
-       
     }
 
+    void withdraw(double amount) throws InsufficientBalanceException {
+        if (amount > 0 && amount <= getBalance()) {
 
-    void withdraw(double amount) throws InsufficientBalanceException{
-        if(amount >0 && amount <=getBalance())
-        {
             deductBalance(amount);
-            
-        }
-        else
-        {
-        
-           throw new InsufficientBalanceException(" Insufficent Balance");
+
+            history.add("Withdrawn: " + amount + " | Current Balance: " + getBalance());
+        } else {
+
+            throw new InsufficientBalanceException("Insufficient Balance or Invalid Amount");
         }
     }
 
-    void display()
-    {
-        System.err.println("Acount Number  : " +getNumber());
-        System.out.println("Accout Holder  : " + getCustomerName());
-        System.out.println("Balance        : " + getBalance());
-        System.err.println(" ");
+    public ArrayList<String> getHistory() {
+
+        return history;
+    }
+
+    // Transfer er jonno alada history record
+    public void addTransferHistory(String note) {
+
+        history.add(note);
+    }
+
+    void display() {
+        System.out.println("Acount Number : " + getNumber() + " | Name: " + getCustomerName() + " | Bal: " + getBalance());
     }
 }
