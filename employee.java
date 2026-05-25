9e5b5ec487e3416d793e1ba6f6ba74c38f1cc32b
saad12092003 <shaadbhuiyan2003@gmail.com>
2026-05-25 10:34:59 +0600

    update employess class use super keyword

--- employee.java
+++ employee.java
@@ -1,6 +1,11 @@
 public class employee extends person
 {
 
+    public employee(String employeeName,String employeeRone) {
+        super(employeeName, employeeRone);
+    }
+     
+
 void display()
 {
      System.err.println("Empolyer Name : "+ employeeName);
