5517fe83163e9a038b7f7d2adb877fef28e8ac72
saad12092003 <shaadbhuiyan2003@gmail.com>
2026-05-25 10:45:11 +0600

    Transaction class a try catch error handling

--- Transaction.java
+++ Transaction.java
@@ -1,11 +1,18 @@
-class Transaction { 
+class Transaction {
+
     public void transfer(Customer sender, Customer receiver, double amount) {
-        if (sender.getBalance() >= amount) {
+
+        try {
+
             sender.withdraw(amount);
+
             receiver.Deposite(amount);
+
             System.out.println("Transfer Success!");
-        } else {
-            System.out.println("Insufficient Balance!");
+
+        } catch (InsufficientBalanceException e) {
+
+            System.out.println(e.getMessage());
         }
     }
 }
